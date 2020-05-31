aa1627bf09086b09d5fc6223c66ace2b70ab3a0e
Lorenzo Maiorfi <maiorfi@innovactive.it>
2020-05-31 17:02:12 +0200

    Test ricerca su un milione di entità indicizzate con 4 indici

--- ScenarioTests/Entities/IndexedObjects1.cs
+++ ScenarioTests/Entities/IndexedObjects1.cs
@@ -1,7 +1,173 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using C5;
+
 namespace ScenarioTests.Entities
 {
-    public class IndexedObjects1
+    public interface IIndexer<Q, R>
     {
-        
+        ICollectionValue<R> this[Q x] { get; }
+    }
+
+    // An index maker has a name, it supports adding and removing items
+    // from the index, and looking up items by key (here of type
+    // Object).
+    public abstract class IndexMaker<T> : IIndexer<object, T>
+    {
+        public string Name { get; }
+        public abstract bool Add(T item);
+        public abstract bool Remove(T item);
+        public abstract ICollectionValue<T> this[object key] { get; }
+
+        public IndexMaker(string name)
+        {
+            Name = name;
+        }
+    }
+
+    // The implementation of an index maker consists of a function to
+    // map an item of type T to the index key type Q, and a dictionary
+    // that maps an index key (of type Q) to a set of items (each of
+    // type T).
+    public class IndexMaker<T, Q> : IndexMaker<T>
+        where T : class
+        where Q : IComparable<Q>
+    {
+        private readonly Func<T, Q> _fun;
+        private readonly TreeDictionary<Q, C5.HashSet<T>> _dictionary;
+
+        public IndexMaker(string name, Func<T, Q> fun) : base(name)
+        {
+            _fun = fun;
+            _dictionary = new TreeDictionary<Q, C5.HashSet<T>>();
+        }
+
+        public override bool Add(T item)
+        {
+            var key = _fun(item);
+            if (!_dictionary.Contains(key))
+            {
+                _dictionary.Add(key, new C5.HashSet<T>(C5.EqualityComparer<T>.Default));
+            }
+
+            return _dictionary[key].Add(item);
+        }
+
+        public override bool Remove(T item)
+        {
+            var key = _fun(item);
+
+            return !_dictionary.Contains(key) ? false : _dictionary[key].Remove(item);
+        }
+
+        public ICollectionValue<T> this[Q key] => _dictionary[key];
+
+        public override ICollectionValue<T> this[object key] => _dictionary[(Q) key];
+
+        public override string ToString()
+        {
+            return _dictionary.ToString();
+        }
+    }
+
+    // Weakly typed implementation of multiple indexers on a class T.  
+
+    // The implementation is an array of index makers, each consisting
+    // of the index's name and its implementation which supports adding
+    // and removing T objects, and looking up T objects by the key
+    // relevant for that index.
+    public class Indexed<T> where T : class
+    {
+        private readonly IndexMaker<T>[] _indexMakers;
+
+        public Indexed(params IndexMaker<T>[] indexMakers)
+        {
+            _indexMakers = indexMakers;
+        }
+
+        public bool Add(T item)
+        {
+            bool result = false;
+
+            foreach (var indexMaker in _indexMakers)
+            {
+                result |= indexMaker.Add(item);
+            }
+
+            return result;
+        }
+
+        public bool Remove(T item)
+        {
+            bool result = false;
+            foreach (var indexMaker in _indexMakers)
+            {
+                result |= indexMaker.Remove(item);
+            }
+
+            return result;
+        }
+
+        public IIndexer<object, T> this[string name]
+        {
+            get
+            {
+                foreach (var indexMaker in _indexMakers)
+                {
+                    if (indexMaker.Name == name)
+                    {
+                        return indexMaker;
+                    }
+                }
+
+                throw new Exception("Unknown index");
+            }
+        }
+
+        // For debugging
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var indexMaker in _indexMakers)
+            {
+                sb.Append("\n----- ").Append(indexMaker.Name).Append("-----\n");
+                sb.Append(indexMaker);
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    // Sample class with two fields but many possible indexes
+    public class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public int Date { get; } // YYYYMMDD as in 20070725
+
+        public Person(string fname, string lname, int date)
+        {
+            FirstName = fname;
+            LastName = lname;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} ({Date})";
+        }
+    }
+
+    // The interface of a strongly typed indexing of PersonIndexed objects:
+    // (Not yet used)
+    public interface IPersonIndexes
+    {
+        IIndexer<string, Person> Name { get; }
+        IIndexer<int, Person> Year { get; }
+        IIndexer<int, Person> Day { get; }
+        IIndexer<string, Person> Month { get; }
     }
 }
--- ScenarioTests/MainTest.cs
+++ ScenarioTests/MainTest.cs
@@ -1,13 +1,93 @@
 using System;
+using System.Linq;
+using Bogus;
+using ScenarioTests.Entities;
 using Xunit;
+using Xunit.Abstractions;
+using Person = ScenarioTests.Entities.Person;
 
 namespace ScenarioTests
 {
-    public class UnitTest1
+    public class MainTester
     {
-        [Fact]
-        public void Test1()
+        private readonly ITestOutputHelper _output;
+
+        public MainTester(ITestOutputHelper output)
         {
+            _output = output;
+        }
+
+        [Fact]
+        public void Test_Indexed_Objects_1()
+        {
+            // Lookup
+            string[] months =
+            {
+                "Jan",
+                "Feb",
+                "Mar",
+                "Apr",
+                "May",
+                "Jun",
+                "Jul",
+                "Aug",
+                "Sep",
+                "Oct",
+                "Nov",
+                "Dec"
+            };
+
+            // definizione indici su entità Person
+            var persons = new Indexed<Person>(
+                new IndexMaker<Person, string>("name", p => p.FirstName),
+                new IndexMaker<Person, int>("year", p => p.Date / 10000),
+                new IndexMaker<Person, int>("day", p => p.Date % 100),
+                new IndexMaker<Person, string>("month", p => months[p.Date / 100 % 100 - 1])
+            );
+
+            var fakerRandomizer = new Bogus.Randomizer();
+            var fakerNameDataset = new Bogus.DataSets.Name("en");
+
+            for (var i = 0; i < 1000000; i++)
+            {
+                persons.Add(new Person(
+                    fakerNameDataset.FirstName(),
+                    fakerNameDataset.LastName(),
+                    fakerRandomizer.Int(1900, 2000) * 10000 + fakerRandomizer.Int(1, 12) * 100 + fakerRandomizer.Int(1, 31)));
+            }
+
+            _output.WriteLine("\nBorn in 1964:");
+
+            if (persons["year"][1964].Any())
+            {
+                foreach (var p in persons["year"][1964])
+                {
+                    _output.WriteLine(p.ToString());
+                }
+            }
+            else _output.WriteLine("None.");
+
+            _output.WriteLine("\nBorn in June:");
+
+            if (persons["month"]["Jun"].Any())
+            {
+                foreach (var p in persons["month"]["Jun"])
+                {
+                    _output.WriteLine(p.ToString());
+                }
+            }
+            else _output.WriteLine("None.");
+
+            _output.WriteLine("\nNamed John:");
+
+            if (persons["name"]["John"].Any())
+            {
+                foreach (var p in persons["name"]["John"])
+                {
+                    _output.WriteLine(p.ToString());
+                }
+            }
+            else _output.WriteLine("None.");
         }
     }
 }
